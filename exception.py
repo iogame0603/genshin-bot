759639a7a45c61d5cb91744acfb5ef23ee657a75
iogame <young312492@gmail.com>
2024-04-04 09:08:02 +0900

    exception 설명 추가

--- exception.py
+++ exception.py
@@ -1,7 +1,9 @@
 from discord.app_commands.errors import AppCommandError
 
 class GenshinCookieException(AppCommandError):
+    """등록되지 않은 쿠키"""
     pass
 
 class GenshinInvalidCookies(AppCommandError):
+    """유효하지 않는 쿠키"""
     pass
