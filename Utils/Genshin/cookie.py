01e4c3b2ff9285952657383a2726d727335e4953
iogame <young312492@gmail.com>
2024-04-04 14:38:10 +0900

    cookie key값 수정

--- Utils/Genshin/cookie.py
+++ Utils/Genshin/cookie.py
@@ -12,9 +12,9 @@ __PRIVATE_KEY = os.getenv("RSA_PRIVATE_KEY")
 def get_cookies(user_id: int) -> Dict[Cookie, Any]:
     cookies = select_cookies(user_id)
     return None if cookies == None else {
-                                            Cookie.LTUID_V2: cookies[0],
-                                            Cookie.LTMID_V2: RSA_CRYPTO.decrypt_msg(__PRIVATE_KEY, cookies[1]),
-                                            Cookie.LTOKEN_V2: RSA_CRYPTO.decrypt_msg(__PRIVATE_KEY, cookies[2]),
+                                            Cookie.LTUID_V2.value: cookies[0],
+                                            Cookie.LTMID_V2.value: RSA_CRYPTO.decrypt_msg(__PRIVATE_KEY, cookies[1]),
+                                            Cookie.LTOKEN_V2.value: RSA_CRYPTO.decrypt_msg(__PRIVATE_KEY, cookies[2]),
                                         }
 
 def get_genshin_client(user_id: int) -> genshin.Client:
