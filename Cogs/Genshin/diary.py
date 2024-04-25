bc9b606e8049a378051ff6056bb898d21f9af698
iogame <young312492@gmail.com>
2024-04-25 21:44:40 +0900

    커맨드 이름 변경 (여행자_핸드북 -> 일지)

--- Cogs/Genshin/diary.py
+++ Cogs/Genshin/diary.py
@@ -32,11 +32,11 @@ class Dirary(commands.Cog):
             os.mkdir(f"{os.path.abspath('')}/images/diary/{channel_id}")
             plt.savefig(f"images/diary/{channel_id}/{user_id}_diary.png", bbox_inches="tight")
 
-    @app_commands.command(name="여행자_핸드북", description="여행자 핸드북을 확인합니다.")
-    @app_commands.describe("month", "확인할 월을 입력합니다. 범위: 현재 월 - 2, 기본값: 현재 월")
+    @app_commands.command(name="일지", description="여행자 핸드북을 확인합니다.")
+    @app_commands.describe(month = "확인할 월을 입력합니다. 범위: 현재 월 - 2, 기본값: 현재 월")
     async def diary(self, interaction: discord.Interaction, month: Optional[int]):
         if month == None:
-            month = datetime.month
+            month = datetime.today().month
 
         try:
             await interaction.response.defer()
@@ -51,7 +51,7 @@ class Dirary(commands.Cog):
             embed.set_thumbnail(url="https://webstatic.hoyoverse.com/upload/op-public/2022/08/04/ff1419346528dfd64d77c35701ecd106_7596171599082743274.png?x-oss-process=image%2Fresize%2Cs_600%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_80")
             file = discord.File(f"{os.path.abspath('')}/images/diary/{interaction.channel_id}/{interaction.user.id}_diary.png", filename=f"{interaction.user.id}_diary.png")
             embed.set_image(url=f"attachment://{interaction.user.id}_diary.png")
-            embed.add_field(name=f"{datetime.today().month}월 원석/모라 수입", value=f"원석: {data.current_primogems}\n모라: {data.current_mora}", inline=False)
+            embed.add_field(name=f"{month}월 원석/모라 수입", value=f"원석: {data.current_primogems}\n모라: {data.current_mora}", inline=False)
             embed.add_field(name="원석 수입 구조", value="", inline=False)
 
             await interaction.followup.send(file=file, embed=embed)
