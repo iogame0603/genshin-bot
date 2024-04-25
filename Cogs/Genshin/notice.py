8d1050abacc7ad57350770c636dc41834e103644
iogame <young312492@gmail.com>
2024-04-26 08:30:04 +0900

    header 수정

--- Cogs/Genshin/notice.py
+++ Cogs/Genshin/notice.py
@@ -31,7 +31,9 @@ def get_notice():
     return List[게시글 이름, 게시글 url]"""
 
     url = "https://cafe.naver.com/MyCafeIntro.nhn?clubid=29893655"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+               "Accept-Encoding": "gzip, deflate, br, zstd"}
     req = requests.get(url=url, headers=headers)
     if req.status_code != 200:
         return [None, req.status_code]
