9d51dca791d09da3259da7b8bc4e079e567d9f62
iogame <young312492@gmail.com>
2024-04-29 09:22:10 +0900

    sqlite3 to mariaDB

--- exception.py
+++ exception.py
@@ -11,3 +11,10 @@ class GenshinCookieException(BaseCookieException):
 class GenshinInvalidCookies(BaseCookieException):
     """유효하지 않는 쿠키"""
     pass
+
+class BaseDataBaseException(AppCommandError):
+    """DB Exception"""
+    pass
+
+class DataBaseException(BaseDataBaseException):
+    """데이터베이스 처리 중 예외 발생"""
